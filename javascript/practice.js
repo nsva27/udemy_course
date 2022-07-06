0635284895875f225901d245f79dd925f7dbc06f
Valery <nesterovsky.valery@gmail.com>
2022-07-06 18:10:58 +0300

    practice of conditions

--- javascript/practice.js
+++ javascript/practice.js
@@ -1,2 +1,29 @@
 'use strict';
 
+// Conditions
+
+const num = 20;
+
+/* if (num < 49) {
+    console.log("Error");
+} else if (num > 100) {
+    console.log("Too much");
+} else {
+    console.log("Bingo!");
+} */
+
+/* switch (num) {
+    case 49:
+        console.log("Very few");
+        break;
+    case 68:
+        console.log("almost few");
+        break;
+    case 70:
+        console.log("bingo!");
+        break;
+    default:
+        console.log("try again");
+} */
+
+// (num < 50) ? console.log("ok") : console.log("error");
--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+/* const numberOfFilms = +prompt("How many movies had you watched", ""); */
+
+/* const personalMovieDB = {
+    count: numberOfFilms,
+    actors: {},
+    movies: {},
+    genres: [],
+    privat: false
+}; */
+
+/* for (let i = 0; i < 2; i++) {
+    const a = prompt("The last movie you watched?", ""),
+          b = prompt("How would you rate it?", "");
+    personalMovieDB.movies[a] = b;
+} */
+
+
